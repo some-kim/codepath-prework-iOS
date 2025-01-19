74198923d40698b969b19541b249bebd2f2b9d5b
Kimone W <kw@crc-dot1x-nat-10-239-96-92.bu.edu>
2025-01-19 16:58:27 -0500

    addded files from x code
    x code files

---
+++ CodePath2025/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  CodePath2025
+//
+//  Created by Kimone W on 1/19/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+        
+    }
+    
+    func changeColor() -> UIColor{
+
+         let red = CGFloat.random(in: 0...1)
+         let green = CGFloat.random(in: 0...1)
+         let blue = CGFloat.random(in: 0...1)
+
+         return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+     }
+    
+    
+    
+    
+}
+
